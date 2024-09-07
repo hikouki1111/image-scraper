a2343efeb51915cf464d4b176f8eee741c467028
kometarou1145 <97835611+hikouki1111@users.noreply.github.com>
2024-09-07 18:43:01 +0900

    add: global checkbox, filename check

--- js/browseraction/popup.js
+++ js/browseraction/popup.js
@@ -16,17 +16,27 @@ function main() {
         if (selectedSet.size != 0) {
             selectedSet.forEach(src => {
                 browser.downloads.download({
-                    url: src
+                    url: src,
+                    filename: getFileName(src)
                 });
             });
         }
+    });
+
+    const globalCheckbox = document.getElementById("global-checkbox");
+    globalCheckbox.addEventListener("change", function() {
+        const allCheckboxElement = document.getElementsByClassName("checkbox");
+        Array.from(allCheckboxElement).forEach((element) => {
+            element.checked = globalCheckbox.checked;
+        });
     });
 }
 
 function update(filter) {
     browser.runtime.sendMessage({ action: "getImageSrc" }).then((response) => {
         if (response.action === "sendImageSrc") {
-            const images = [...new Set(response.imageSrc)].filter(src => filter === "" || includesIgnoreCase(src, filter));
+            const images = [...new Set(response.imageSrc)]
+                .filter(src => filter === "" || includesIgnoreCase(src, filter));
             const containerElement = document.getElementById("image-container");
             containerElement.innerHTML = "";
             const counterElement = document.getElementById("counter");
@@ -69,6 +79,11 @@ function update(filter) {
 
 function includesIgnoreCase(string1, string2) {
     return string1.toLowerCase().includes(string2.toLowerCase());
+}
+
+function getFileName(url) {
+    const pathname = new URL(url).pathname;
+    return pathname.substring(pathname.lastIndexOf('/') + 1);
 }
 
 main();
